ab4525f2ac70b464093d2e0b4dae80bdc9dd4ff6
Vishal Jituri <25f1002229@ds.study.iitm.ac.in>
2025-08-07 00:08:24 +0530

    Update hello.controller.ts

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class HelloController {
   @Get()
   getHello(): { message: string } {
-    return { message: 'Hello World' };
+    return { message: 'Hello World! Welcome to Schedula Backend!' };
   }
 }
