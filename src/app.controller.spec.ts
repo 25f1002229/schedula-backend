1ba642c152abb6932691b31ac9541c41beab7a96
Vishal Jituri <25f1002229@ds.study.iitm.ac.in>
2025-08-07 01:02:41 +0530

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World! Welcome to Schedula Backend!"', () => {
-      expect(appController.getHello()).toBe('Hello World! Welcome to Schedula Backend!');
+      expect(appController.getHello()).toBe('Welcome to Schedula Backend!');
     });
   });
 });
